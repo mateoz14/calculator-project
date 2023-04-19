c0781449475263356d9b11664e7ec5c59df1a140
Mateo <mateozguppy@gmail.com>
2023-04-19 18:38:20 -0400

    functionable addition operand but only with two numbers

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let output = document.querySelector(".output-box")
 let oneButton = document.querySelector(".one")
+let equalButton = document.querySelector('.equal')
+let plusButton = document.querySelector('.plus')
 
-function numberOutput(number) {
-    output.textContent += number;
-};
+let currentNumber = ""
+let nextNumber = ""
+let whichNumber = 0
 
-oneButton.addEventListener('click', numberOutput("1"));
+
+oneButton.addEventListener('click', () => {
+    output.textContent += "1";
+});
+
+plusButton.addEventListener('click', () => {
+    currentNumber = +output.textContent
+    output.textContent = ""
+});
+
+equalButton.addEventListener('click', () => {
+    nextNumber = +output.textContent
+    output.textContent = `${currentNumber + nextNumber}`
+}); // this works but only if there are two numbers put in
