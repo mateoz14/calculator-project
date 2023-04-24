00810f54b25e51c496185c23cdf5c0dcbf1bddaf
Mateo <mateozguppy@gmail.com>
2023-04-24 15:30:43 -0400

    Addition function is finally working, Key takeaway: store variables that will change through functions as objects.

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
 let output = document.querySelector(".output-box")
 let oneButton = document.querySelector(".one")
 let equalButton = document.querySelector('.equal')
-let plusButton = document.querySelector('.plus')
+let addButton = document.querySelector('.add')
+let multiply = document.querySelector('.mult');
 
-let currentNumber = ""
-let currentAnswer = 0
+let currentNumber = "";
+let numbers = {currentNumber: "", currentAnswer: 0, chosenOperator: 0};
 // ^ holds a variable for the current answer inside of the calculator so
 // we can use multiple numbers.
 
 oneButton.addEventListener('click', () => {
     output.textContent += "1";
-    currentNumber += "1";
+    numbers.currentNumber += "1";
     // adds number user clicks to the current number.
 });
 
-plusButton.addEventListener('click', () => {
+function addition() {
     output.textContent += " + ";
-    currentAnswer += +currentNumber;
-    // sets the current answer to equal the currentNumber
-    currentNumber = "";
-    // resets the current number
+    if (numbers.currentNumber == "") {
+        // do nothing
+    } else {
+        numbers.currentAnswer += parseInt(numbers.currentNumber)
+    };
+    numbers.currentNumber = "";
+    numbers.chosenOperator = addition;
+};
+
+addButton.addEventListener('click', () => {
+    addition();
 })
 
+function operate(operator) {
+    operator()
+    output.textContent = `${numbers.currentAnswer}`
+}
+
 equalButton.addEventListener('click', () => {
-    currentAnswer += +currentNumber;
-    // currentAnswer will display x + y.
-    output.textContent = `${currentAnswer}`;
-    currentAnswer = 0;
-    currentNumber = "";
-    // resets all.
+   operate(numbers.chosenOperator)
+   
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
